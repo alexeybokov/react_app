2bca22d0e3351fe614db732589a07a03b02c7259
alexeybokov <lexabokov18@gmail.com>
2019-04-22 19:52:40 +0300

    lesson 86

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -18,7 +18,7 @@ import classes from './Cockpit.module.css';
 
     return(
       <div className={classes.Cockpit}>
-        <h1>Hi, I'm React App</h1>
+        <h1>{props.title}</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
           className={btnClass}
